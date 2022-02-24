e14b3f697e67d4d35a4fc80f41b40448b5f572d4
abdulawalemon1 <emondx4@gmail.com>
2022-02-25 03:28:02 +0600

    card displayed

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 //get input value
 //button event handler
 //error handling for string value
+const main = document.getElementById('main');
 const searchButton = () => {
     console.log('button clicked')
     const input = document.getElementById('input-value');
-    let inputValue = input.value;
+    let inputValue = parseInt(input.value);
     const error = document.getElementById('error');
     if (isNaN(inputValue) || inputValue == '') {
         error.innerText = 'Please enter a number!'
@@ -12,5 +13,29 @@ const searchButton = () => {
     } else if (inputValue <= 0) {
         error.innerText = 'Please enter a positive number!';
         input.value = '';
+    } else {
+        fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${inputValue}`)
+            .then(res => res.json())
+            .then(data => cardDisplay(data.cards))
+    }
+}
+const cardDisplay = cards => {
+    for (const card of cards) {
+        console.log(card);
+        const div = document.createElement('div');
+        div.classList.add('col-lg-4');
+        div.classList.add('mb-5');
+        div.innerHTML = `
+        <div class="card" style="width: 18rem;">
+        <img src="${card.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+            `;
+        main.appendChild(div);
+
     }
 }
